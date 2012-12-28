5d6c917a03654defd23059319de90c6025ac5fe7
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-12-28 18:22:30 +0100

    Initial tests

--- troia_client/test.py
+++ troia_client/test.py
@@ -1,16 +1,61 @@
-import sys
+import unittest
 
 from client import TroiaClient
 
 
-if __name__ == '__main__':
+ADRESS = 'http://localhost:8080/troia-server-0.0.1'
 
-    if len(sys.argv) > 1:
-        address = sys.argv[1]
-    else:
-        address = 'http://localhost:8080/troia-server-0.0.1'
-    tc = TroiaClient(address, "TEST_JOB_")
-    print "PING:", tc.ping()
-    print "PINGDB:", tc.pingDB()
-    print "CREATE:", tc.create("batch")
-    print "INFO:", tc.info()
+
+class TroiaClientTestBase(unittest.TestCase):
+
+    def setUp(self):
+        self.tc = TroiaClient(ADRESS, self.JOB_ID)
+
+
+class TestJobManipulation(TroiaClientTestBase):
+
+    JOB_ID = "TESTING_JOB_MANIUPULATION"
+
+    def setUp(self):
+        super(TestJobManipulation, self).setUp()
+        self.tc.delete()
+
+    def test_creation(self):
+        w = self.tc.info()
+        self.assertEqual('ERROR', w['status'])
+        w = self.tc.create()
+        self.assertEqual('OK', w['status'])
+        w = self.tc.info()
+        self.assertEqual('OK', w['status'])
+
+    def test_deletion(self):
+        w = self.tc.info()
+        self.assertEqual('ERROR', w['status'])
+        w = self.tc.create()
+        self.assertEqual('OK', w['status'])
+        w = self.tc.info()
+        self.assertEqual('OK', w['status'])
+        w = self.tc.delete()
+        self.assertEqual('OK', w['status'])
+        w = self.tc.info()
+        self.assertEqual('ERROR', w['status'])
+
+
+class TestStatus(TroiaClientTestBase):
+
+    JOB_ID = "TESTING_OVERAL_STATUSES"
+
+    def setUp(self):
+        super(TestStatus, self).setUp()
+
+    def test_ping(self):
+        w = self.tc.ping()
+        self.assertTrue('OK', w['status'])
+
+    def test_pingDB(self):
+        w = self.tc.pingDB()
+        self.assertTrue('OK', w['status'])
+
+
+if __name__ == '__main__':
+    unittest.main()
