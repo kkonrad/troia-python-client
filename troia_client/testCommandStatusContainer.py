c15540cf16312fe1d511008e2f1137758afee664
Artur Ambroziak <artur.ambroziak@10clouds.com>
2013-01-25 13:27:38 +0100

    testing SerializedCommandStatusContainer

---
+++ troia_client/testCommandStatusContainer.py
@@ -0,0 +1,25 @@
+import unittest
+from client import TroiaClient
+from testSettings import TestSettings
+from test_on_data import COST_MATRIX, WORKERS_LABELS
+import time
+
+class TestStatus(unittest.TestCase):
+
+        def setUp(self):
+            self.tc = TroiaClient(TestSettings.ADDRESS, "test")
+            
+        def test_GetRedirectData(self):
+            try:
+                self.tc.delete()
+                time.sleep(1)
+            except:
+                pass
+            self.tc.create(COST_MATRIX)
+            assigend_labels_response = self.tc.get_assigned_labels()
+            time.sleep(0.5)
+            self.assertDictEqual(self.tc.get_status(assigend_labels_response['redirect'])['result'], {})
+            self.tc.await_completion(self.tc.post_assigned_labels(WORKERS_LABELS))
+            self.assertIsNotNone(self.tc.await_completion(self.tc.get_assigned_labels())['result'])
+            #still empty
+            self.assertDictEqual(self.tc.get_status(assigend_labels_response['redirect'])['result'], {})
