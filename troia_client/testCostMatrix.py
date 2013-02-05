bdf827f768f13b4565bcbe33692b444ec1895a32
Dana Mitrea <dana.mitrea@gmail.com>
2013-02-05 11:03:51 +0200

    Removed the predefined jobId from the CostMatrix tests

--- troia_client/testCostMatrix.py
+++ troia_client/testCostMatrix.py
@@ -10,18 +10,14 @@ class TestCostMatrix(unittest.TestCase):
             return "".join([random.choice(string.ascii_lowercase) for x in xrange(length)])
         
         def test_CostMatrix_01Values(self):
-            jobId = self.generateId(10)
             categories = [{"name":"porn", "prior":0.3, "misclassificationCost": [{'categoryName': 'porn', 'value': 0.0}, {'categoryName': 'notporn', 'value': 1.0}]}, 
                           {"name":"notporn", "prior":0.7, "misclassificationCost":[{'categoryName': 'porn', 'value': 1.0}, {'categoryName': 'notporn', 'value': 0.0}]}]
-            client = TroiaClient(TestSettings.ADDRESS, jobId)
+            client = TroiaClient(TestSettings.ADDRESS)
             response = client.createNewJob(categories)
             self.assertEqual('OK', response['status'])
-            self.assertEqual('New job created with ID: ' + jobId, response['result'])
+            self.assertEqual('New job created with ID: ' + client.jid, response['result'])
             
-            response = client.get_cost_matrix()
-            self.assertEqual('OK', response['status'])
-            command_id = response['redirect']
-            response = client.get_status(command_id)
+            response = client.await_completion(client.get_cost_matrix())
             self.assertEqual('OK', response['status'])
             result = response['result']
             self.assertEqual(len(categories), len(result))
@@ -30,18 +26,14 @@ class TestCostMatrix(unittest.TestCase):
                 self.assertTrue(category in str(categories))
             
         def test_CostMatrix_DoubleValues(self):
-            jobId = self.generateId(10)
             categories = [{"name":"porn", "prior":0.3, "misclassificationCost": [{'categoryName': 'porn', 'value': 0.4}, {'categoryName': 'notporn', 'value': 0.6}]}, 
                           {"name":"notporn", "prior":0.7, "misclassificationCost":[{'categoryName': 'porn', 'value': 0.6}, {'categoryName': 'notporn', 'value': 0.4}]}]
-            client = TroiaClient(TestSettings.ADDRESS, jobId)
+            client = TroiaClient(TestSettings.ADDRESS)
             response = client.createNewJob(categories)
             self.assertEqual('OK', response['status'])
-            self.assertEqual('New job created with ID: ' + jobId, response['result'])
+            self.assertEqual('New job created with ID: ' + client.jid, response['result'])
             
-            response = client.get_cost_matrix()
-            self.assertEqual('OK', response['status'])
-            command_id = response['redirect']
-            response = client.get_status(command_id)
+            response = client.await_completion(client.createNewJob(categories))
             self.assertEqual('OK', response['status'])
             result = response['result']
             self.assertEqual(len(categories), len(result))
@@ -51,29 +43,23 @@ class TestCostMatrix(unittest.TestCase):
                 
         def test_UpdateCostMatrix(self):
             #create a job with some default categories
-            jobId = self.generateId(10)
             categories = [{"name":"porn", "prior":0.5, "misclassificationCost": [{'categoryName': 'porn', 'value': 0.0}, {'categoryName': 'notporn', 'value': 1.0}]}, 
                           {"name":"notporn", "prior":0.5, "misclassificationCost":[{'categoryName': 'porn', 'value': 1.0}, {'categoryName': 'notporn', 'value': 0.0}]}]
-            client = TroiaClient(TestSettings.ADDRESS, jobId)
+            client = TroiaClient(TestSettings.ADDRESS)
             response = client.createNewJob(categories)
             self.assertEqual('OK', response['status'])
-            self.assertEqual('New job created with ID: ' + jobId, response['result'])
+            self.assertEqual('New job created with ID: ' + client.jid, response['result'])
             
             #update the cost matrix
             newCostMatrix = [{"categoryTo":"porn", "cost":0.3, "categoryFrom":"porn"}, {"categoryTo":"porn", "cost":1.7, "categoryFrom":"notporn"}, {"categoryTo":"notporn", "cost":1.7, "categoryFrom":"porn"}, {"categoryTo":"notporn", "cost":0.3, "categoryFrom":"notporn"}]
             
-            response = client.post_cost_matrix(newCostMatrix)
-            self.assertEqual('OK', response['status'])
-            command_id = response['redirect']
-            response = client.get_status(command_id)
+            response = client.await_completion(client.post_cost_matrix(newCostMatrix))
             self.assertEqual('OK', response['status'])
             self.assertEqual('Costs set', response['result'])
             
             #retrieve the cost matrix and check that it is updated correctly
-            response = client.get_cost_matrix()
+            response = client.await_completion(client.get_cost_matrix())
             self.assertEqual('OK', response['status'])
-            command_id = response['redirect']
-            response = client.get_status(command_id)
             result = response['result']
             expectedCategories = [{'prior':0.5, 'misclassificationCost': [{'categoryName': 'porn', 'value': 0.3}, {'categoryName': 'notporn', 'value': 1.7}], 'name':'porn'}, 
                                   {'prior':0.5, 'misclassificationCost': [{'categoryName': 'porn', 'value': 1.7}, {'categoryName': 'notporn', 'value': 0.3}], 'name':'notporn'}]
