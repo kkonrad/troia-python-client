2dc811b68a4a9c855fc612db3905a1b641028448
Artur Ambroziak <artur.ambroziak@10clouds.com>
2013-02-19 13:25:18 +0100

    testCategories refactor

--- troia_client/testCategories.py
+++ troia_client/testCategories.py
@@ -12,70 +12,41 @@ class TestCategories(unittest.TestCase):
         def tearDown(self):
             self.client.delete()
 
-        def test_AddGetCategories_PrintableASCII_SpecialChars(self):
-            categories = [{"prior":0.5, "name":"!@#$:;,.{}[]"}, {"prior":0.5, "name":"2ndCategory"}]
+        def _test_method(self, categories, expected_categories):
             response = self.client.create(categories)
             self.assertEqual('OK', response['status'])
 
             response = self.client.await_completion(self.client.get_categories())
             self.assertEqual('OK', response['status'])
-            expectedCategories = [u'!@#$:;,.{}[]', u'2ndCategory']
-            for category in expectedCategories:
+            for category in expected_categories:
                 self.assertTrue(category in response['result'])
+
+        def test_AddGetCategories_PrintableASCII_SpecialChars(self):
+            categories = [{"prior":0.5, "name":"!@#$:;,.{}[]"}, {"prior":0.5, "name":"2ndCategory"}]
+            expectedCategories = [u'!@#$:;,.{}[]', u'2ndCategory']
+            self._test_method(categories, expectedCategories)
 
         def test_AddGetCategories_ExtendedASCIIChars(self):
             categories = [{"prior":0.5, "name":"œŒ"}, {"prior":0.5, "name":"ÀÆË™ž¤©"}]
-            response = self.client.create(categories)
-            self.assertEqual('OK', response['status'])
-
-            response = self.client.await_completion(self.client.get_categories())
-            self.assertEqual('OK', response['status'])
-
             expectedCategories = [u'œŒ', u'ÀÆË™ž¤©']
-            for category in expectedCategories:
-                self.assertTrue(category in response['result'])
+            self._test_method(categories, expectedCategories)
 
         def test_AddGetCategories_UnicodeChars(self):
             categories = [{"prior":0.5, "name":"ૉେஇΨҖӖմ؂څ"}, {"prior":0.5, "name":"ూഹܬआਖ਼"}]
-            response = self.client.create(categories)
-            self.assertEqual('OK', response['status'])
-
-            response = self.client.await_completion(self.client.get_categories())
-            self.assertEqual('OK', response['status'])
-
             expectedCategories = [u'ૉେஇΨҖӖմ؂څ', u'ూഹܬआਖ਼']
-            for category in expectedCategories:
-                self.assertTrue(category in response['result'])
+            self._test_method(categories, expectedCategories)
 
         def test_CostMatrix_01Values(self):
             categories = [{"name":"porn", "prior":0.3, "misclassificationCost": [{'categoryName': 'porn', 'value': 0.0}, {'categoryName': 'notporn', 'value': 1.0}]},
                           {"name":"notporn", "prior":0.7, "misclassificationCost":[{'categoryName': 'porn', 'value': 1.0}, {'categoryName': 'notporn', 'value': 0.0}]}]
-            response = self.client.create(categories)
-            self.assertEqual('OK', response['status'])
-            self.assertEqual('New job created with ID: ' + self.client.jid, response['result'])
-
-            response = self.client.await_completion(self.client.get_cost_matrix())
-            self.assertEqual('OK', response['status'])
-            self.assertEqual(len(categories), len(response['result']))
-            expectedCategories = [{u'prior': 0.3, u'misclassificationCost': [{u'categoryName': u'porn', u'value': 0.0}, {u'categoryName': u'notporn', u'value': 1.0}], u'name': u'porn'},
-                                  {u'prior': 0.7, u'misclassificationCost': [{u'categoryName': u'porn', u'value': 1.0}, {u'categoryName': u'notporn', u'value': 0.0}], u'name': u'notporn'}]
-            for result in response['result']:
-                self.assertTrue(result in expectedCategories)
+            expectedCategories = ['porn', 'notporn']
+            self._test_method(categories, expectedCategories)
 
         def test_CostMatrix_DoubleValues(self):
             categories = [{"name":"porn", "prior":0.3, "misclassificationCost": [{'categoryName': 'porn', 'value': 0.4}, {'categoryName': 'notporn', 'value': 0.6}]},
                           {"name":"notporn", "prior":0.7, "misclassificationCost":[{'categoryName': 'porn', 'value': 0.6}, {'categoryName': 'notporn', 'value': 0.4}]}]
-            response = self.client.create(categories)
-            self.assertEqual('OK', response['status'])
-            self.assertEqual('New job created with ID: ' + self.client.jid, response['result'])
-
-            response = self.client.await_completion(self.client.get_cost_matrix())
-            self.assertEqual('OK', response['status'])
-            self.assertEqual(len(categories), len(response['result']))
-            expectedCategories = [{u'prior': 0.3, u'misclassificationCost': [{u'categoryName': u'porn', u'value': 0.4}, {u'categoryName': u'notporn', u'value': 0.6}], u'name': u'porn'},
-                                  {u'prior': 0.7, u'misclassificationCost': [{u'categoryName': u'porn', u'value': 0.6}, {u'categoryName': u'notporn', u'value': 0.4}], u'name': u'notporn'}]
-            for result in response['result']:
-                self.assertTrue(result in expectedCategories)
+            expectedCategories = ['porn', 'notporn']
+            self._test_method(categories, expectedCategories)
 
         def test_UpdateCostMatrix(self):
             #create a job with some default categories
@@ -100,6 +71,3 @@ class TestCategories(unittest.TestCase):
                                   {u'prior': 0.5, u'misclassificationCost': [{u'categoryName': u'porn', u'value': 1.7}, {u'categoryName': u'notporn', u'value': 0.3}], u'name': u'notporn'}]
             for result in response['result']:
                 self.assertTrue(result in expectedCategories)
-
-
-
