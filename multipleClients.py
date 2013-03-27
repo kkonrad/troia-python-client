5ffa4556dda261acb5bf400e75e1913882924504
Konrad Kurdej <konrad.kurdej@10clouds.com>
2013-03-27 18:02:17 +0100

    Adding checks in multipleClients

--- multipleClients.py
+++ multipleClients.py
@@ -6,21 +6,37 @@ from threading import Thread
 from random import randint
 
 
+def check_status(response):
+    if response['status'] == 'ERROR':
+        print response['result'], response.get('stacktrace')
+    return response
+
+
+def check_assigns(client, expected):
+    w = check_status(client.await_completion(client.get_assigned_labels()))
+    if len(w['result']) != expected:
+        print "WRONG NUMBER OF ASSIGNS %d, expected %d" % (len(w['result']), expected)
+
+
 def createNominalJob():
     client = TroiaClient(ADDRESS)
     client.create(CATEGORIES)
     jobs.append((client.jid, "NOMINAL"))
-    client.await_completion(client.post_assigned_labels(ASSIGNED_LABELS))
-    client.await_completion(client.post_evaluation_objects(EVALUATION_DATA))
-    client.await_completion(client.post_compute())
+    check_status(client.await_completion(client.post_assigned_labels(ASSIGNED_LABELS)))
+    check_status(client.await_completion(client.post_evaluation_objects(EVALUATION_DATA)))
+    check_status(client.await_completion(client.post_compute()))
+    check_assigns(client, len(ASSIGNED_LABELS))
+
 
 
 def createContJob():
     client = TroiaContClient(ADDRESS)
     client.create()
     jobs.append((client.jid, "CONTINUOUS"))
-    client.await_completion(client.post_assigned_labels(ASSIGNED_LABELS_CONT))
-    client.await_completion(client.post_gold_data(GOLD_LABELS_CONT))
+    check_status(client.await_completion(client.post_assigned_labels(ASSIGNED_LABELS_CONT)))
+    check_status(client.await_completion(client.post_gold_data(GOLD_LABELS_CONT)))
+    check_assigns(client, len(ASSIGNED_LABELS_CONT))
+
 
 if __name__ == '__main__':
     NoThreads = 300
