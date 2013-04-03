08b4ac759cc08195ec63f771838dad3913faf794
Dana Mitrea <dana.mitrea@gmail.com>
2013-04-03 07:15:56 +0300

    Updated TestCachedScheduler suite

--- nominalJobsTests/testScheduler.py
+++ nominalJobsTests/testScheduler.py
@@ -7,29 +7,32 @@ from testSettings import *
 
 class TestCachedScheduler(unittest.TestCase):
 
-    def test_IMV(self):
+    def setUp(self):
+        self.client = TroiaClient(ADDRESS)
+
+    def tearDown(self):
+        self.client.delete()
+
+    def test_IMV_CachedScheduler_CountAssignsCalculator(self):
         self._test_method('IMV')
 
-    def test_IDS(self):
-        # TODO Incremental Dawid Skene
-        # self._test_method('IDS')
-        pass
+    def test_IDS_CachedScheduler_CountAssignsCalculator(self):
+        self._test_method('IDS')
 
-    def test_BMV(self):
+    def test_BMV_CachedScheduler_CountAssignsCalculator(self):
         self._test_method('BMV')
 
-    def test_BDS(self):
+    def test_BDS_CachedScheduler_CountAssignsCalculator(self):
         self._test_method('BDS')
 
     def _test_method(self, algorithm):
-        client = TroiaClient(ADDRESS)
-        response = client.create(
+        response = self.client.create(
             CATEGORIES,
             categoryPriors=CATEGORY_PRIORS,
             costMatrix=COST_MATRIX,
             algorithm=algorithm,
-            scheduler="cachedscheduler",
-            calculator="countassigns"
+            scheduler='cachedscheduler',
+            calculator='countassigns'
         )
         self.assertEqual('OK', response['status'])
         num_objects = 3
@@ -41,22 +44,24 @@ class TestCachedScheduler(unittest.TestCase):
                     'object{}'.format(i),
                     CATEGORIES[random.randint(0, len(CATEGORIES) - 1)]
                 ))
-        print assigns
-        self.assertEqual('OK', client.await_completion(client.post_assigned_labels(assigns))['status'])
-        self.assertEqual('OK', client.await_completion(client.post_compute())['status'])
+        self.assertEqual('OK', self.client.await_completion(self.client.post_assigned_labels(assigns))['status'])
+        self.assertEqual('OK', self.client.await_completion(self.client.post_compute())['status'])
 
         # Get created objects.
-        objects = client.await_completion(client.get_objects())['result']
+        objects = self.client.await_completion(self.client.get_objects())['result']
+        self.assertEquals(num_objects, len(objects))
+        #we have object0 = 3 assigns, object1 = 6 assigns, object2 = 9 assigns
 
-        # Get all objects from the queue.
+        #retrieve the objects from the queue:
+        expectedObjectsQueue = ['object2', 'object1', 'object0']
         for i in xrange(num_objects):
-            print objects[i]['name']
-            self.assertEqual( objects[i]['name'], client.await_completion(client.get_next_object())['result']['name'])
-        # This one should be null. That means the 'result' key is not present
-        # in the response.
-        self.assertIsNone(client.await_completion(client.get_next_object()).get('result', None))
+            response = self.client.await_completion(self.client.get_next_object())
+            self.assertEqual(expectedObjectsQueue[i], self.client.await_completion(self.client.get_next_object())['result']['name'])
+
+        # This one should be null. That means the 'result' key is not present in the response.
+        self.assertIsNone(self.client.await_completion(self.client.get_next_object()).get('result', None))
+
         # Add assign to the object. The object should be evicted from the
         # cache and returned by subsequent 'nextObject' call.
-        self.assertEqual('OK', client.await_completion(client.post_assigned_labels(assigns[-1:]))['status'])
-        self.assertEqual(objects[-1]['name'], client.await_completion(client.get_next_object())['result']['name'])
-        client.delete()
+        self.assertEqual('OK', self.client.await_completion(self.client.post_assigned_labels(assigns[-1:]))['status'])
+        self.assertEqual(objects[-1]['name'], self.client.await_completion(self.client.get_next_object())['result']['name'])
