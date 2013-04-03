cd1f1c2b92b0f3f81eba118fded7f20c9dfbec83
Dana Mitrea <dana.mitrea@gmail.com>
2013-04-03 06:08:11 +0300

    Updated testCategories - Added test_CreateJobWithCostMatrix_GetCostMatrix and test_CreateJobWithoutCostMatrix_3Categories_GetCostMatrix

--- nominalJobsTests/testCategories.py
+++ nominalJobsTests/testCategories.py
@@ -24,12 +24,6 @@ class TestCategories(unittest.TestCase):
         def _create_priors(self, categories, priors):
             return [{'categoryName': c, "value": p} for c, p in zip(categories, priors)]
 
-        def test_AddGetCategories_SameCategoryNames(self):
-            categories = [u'category1', u'category1']
-            response = self.client.create(categories)
-            self.assertEqual('ERROR', response['status'])
-            self.assertEqual('Category names should be different', response['result'])
-
         def test_AddGetCategories_LongCategoryNames(self):
             categories = [u'hjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdh', u'hjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhfhdfgjkshfghdsfkgjldkgjfdkgjdflgjfkdljajdghafdhjkdhsdsd']
             self._test_method(categories)
@@ -53,6 +47,8 @@ class TestCategories(unittest.TestCase):
 
             response = self.client.await_completion(self.client.get_categories())
             self.assertEqual('OK', response['status'])
+            for category in categories:
+                self.assertTrue(category in response['result'])
 
         def test_CreateJobWithCategoryPriors_GetPriors(self):
             categories = [u'category1', u'category2']
@@ -62,22 +58,60 @@ class TestCategories(unittest.TestCase):
 
             response = self.client.await_completion(self.client.get_categories())
             self.assertEqual('OK', response['status'])
+            for category in categories:
+                self.assertTrue(category in response['result'])
 
-        def test_CreateJobWithoutCostMatrix_GetCostMatrix(self):
+        def test_CreateJobWithoutCostMatrix_2Categories_GetCostMatrix(self):
             categories = [u'category1', u'category2']
             response = self.client.create(categories)
             self.assertEqual('OK', response['status'])
 
             response = self.client.await_completion(self.client.get_cost_matrix())
             self.assertEqual('OK', response['status'])
-            print response
 
-        def test_CreateJobWithCostMatrix_GetCostMatrix(self):
-            categories = [u'category1', u'category2']
-            response = self.client.create(categories, costMatrix=COST_MATRIX)
+            #check that the default 0-1 cost matrix is created
+            for cat1 in categories:
+                for cat2 in categories:
+                        if cat1 != cat2:
+                            self.assertEqual(1.0, response['result'][cat1][cat2])
+                        else:
+                            self.assertEqual(0.0, response['result'][cat1][cat2])
+
+        def test_CreateJobWithoutCostMatrix_3Categories_GetCostMatrix(self):
+            categories = [u'category1', u'category2', u'category3']
+            response = self.client.create(categories)
             self.assertEqual('OK', response['status'])
 
             response = self.client.await_completion(self.client.get_cost_matrix())
             self.assertEqual('OK', response['status'])
-            print response
+
+            #check that the default 0-1 cost matrix is created
+            for cat1 in categories:
+                for cat2 in categories:
+                    for cat3 in categories:
+                        if ((cat1 != cat2) and (cat2 != cat3) and (cat1 != cat3)):
+                            self.assertEqual(1.0, response['result'][cat1][cat2])
+                            self.assertEqual(1.0, response['result'][cat1][cat3])
+                        else:
+                            self.assertEqual(0.0,response['result'][cat1][cat1])
+
+        def test_CreateJobWithCostMatrix_GetCostMatrix(self):
+            categories = [u'category1', u'category2', u'category3']
+            cMatrix = {"category1":{"category1":0.0, "category2":0.5, "category3":0.5},
+                       "category2":{"category1":0.5, "category2":0.0, "category3":0.5},
+                       "category3":{"category1":0.5, "category2":0.5, "category3":0.0}} 
+            response = self.client.create(categories, cMatrix=cMatrix)
+            self.assertEqual('OK', response['status'])
+
+            response = self.client.await_completion(self.client.get_cost_matrix())
+            self.assertEqual('OK', response['status'])
+            for cat1 in categories:
+                for cat2 in categories:
+                    for cat3 in categories:
+                        if ((cat1 != cat2) and (cat2 != cat3) and (cat1 != cat3)):
+                            self.assertEqual(0.5, response['result'][cat1][cat2])
+                            self.assertEqual(0.5, response['result'][cat1][cat3])
+                        else:
+                            self.assertEqual(0.0, response['result'][cat1][cat1])
+
 
