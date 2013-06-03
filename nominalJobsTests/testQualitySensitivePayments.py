b58c3c9896cceef4c6556ee807757bb77a664379
Dana Mitrea <dana.mitrea@gmail.com>
2013-06-03 18:00:32 +0300

    Added QSP tests for 0 and negative worker quality

--- nominalJobsTests/testQualitySensitivePayments.py
+++ nominalJobsTests/testQualitySensitivePayments.py
@@ -1,6 +1,7 @@
 import unittest
 from client.gal import TroiaClient
 from testSettings import *
+from pprint import pprint
 
 
 class TestQualitySensitivePayments(unittest.TestCase):
@@ -10,7 +11,7 @@ class TestQualitySensitivePayments(unittest.TestCase):
     def tearDown(self):
         self.client.delete()
 
-    def testQSP(self):
+    def testQSP_ZeroWorkerQUality(self):
         assignedLabels = [
                             ('worker1', 'url1', 'cat2'),
                             ('worker1', 'url2', 'cat1'),
@@ -34,5 +35,48 @@ class TestQualitySensitivePayments(unittest.TestCase):
         response = self.client.post_compute()
         self.assertEqual('OK', response['status'])
 
+        response = self.client.await_completion(self.client.get_estimated_workers_quality("ExpectedCost"))
+        self.assertEqual('OK', response['status'])
+        pprint(response)
+
         response = self.client.await_completion(self.client.get_workers_quality_payment())
-        print response
+        pprint(response)
+
+    def testQSP_NegativeWorkerQuality(self):
+        assignedLabels = [
+                            ('worker1', 'url1', 'cat2'),
+                            ('worker1', 'url2', 'cat1'),
+                            ('worker1', 'url3', 'cat2'),
+                            ('worker2', 'url1', 'cat1'),
+                            ('worker2', 'url2', 'cat1'),
+                            ('worker2', 'url3', 'cat1'),
+                            ('worker3', 'url1', 'cat1'),
+                            ('worker3', 'url2', 'cat2'),
+                            ('worker3', 'url3', 'cat1')
+                            ]
+        goldLabels = [
+                      ('url1', 'cat1'),
+                      ('url2', 'cat2'),
+                      ('url3', 'cat1')
+                      ]
+        response = self.client.create(["cat1", "cat2"], categoryPriors= [{"categoryName": "cat1", "value": 0.1}, {"categoryName": "cat2", "value": 0.9}])
+        self.assertEqual('OK', response['status'])
+
+        response = self.client.await_completion(self.client.post_assigned_labels(assignedLabels))
+        self.assertEqual('OK', response['status'])
+
+        response = self.client.await_completion(self.client.post_gold_data(goldLabels))
+        self.assertEqual('OK', response['status'])
+
+        response = self.client.post_compute()
+        self.assertEqual('OK', response['status'])
+
+        response = self.client.await_completion(self.client.get_workers_confusion_matrix())
+        pprint(response)
+
+        response = self.client.await_completion(self.client.get_estimated_workers_quality("ExpectedCost"))
+        self.assertEqual('OK', response['status'])
+        pprint(response)
+
+        response = self.client.await_completion(self.client.get_workers_quality_payment())
+        pprint(response)
